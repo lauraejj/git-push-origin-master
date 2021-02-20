1b51e8cb3c4115e2312204beec780d711ce7cf86
laura julien <laura.julien@ymail.com>
2021-02-20 11:35:12 +0100

    la loose

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :restaurants, only: [ :index, :show,:new, :create ] do
+    resources :reviews,  only: [ :new, :create ] # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+end#
+
+Rails.application.routes.draw do
+  get "restaurants", to: "restaurants#index"
+end
+Rails.application.routes.draw do
+  get "restaurants/new", to: "restaurants#new" # The `new` route needs to be *before* `show` route.
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+Restaurant.destroy_all
 #   Character.create(name: 'Luke', movie: movies.first)
+tokyo = Restaurant.create([{ name: 'Tokyo' }, { address: '75016 Paris' }, { category: 'japanese' }])
+napoli = Restaurant.create([{ name: 'Napoli' }, { address: '75012 Paris' }, { category: 'italian' }])
+peking = Restaurant.create([{ name: 'Peking' }, { address: '75003 Paris' }, { category: 'chinese' }])
+paris = Restaurant.create([{ name: 'Paris' }, { address: '75001 Paris' }, { category: 'french' }])
+bruges = Restaurant.create([{ name: 'Bruges' }, { address: '75002 Paris' }, { category: 'belgian' }])
